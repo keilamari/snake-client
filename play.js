d76e3cfcb9c2654549952ead8de3f60d6b2cd94b
Keila Kopvillem <kmkopvillem@gmail.com>
2022-02-16 15:27:51 -0500

    added exit key

--- play.js
+++ play.js
@@ -25,6 +25,20 @@ const connect = function() {
 
 console.log("Connecting ...");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  const handleUserInput = function() {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+  return stdin;
+};
+
 
 
 
